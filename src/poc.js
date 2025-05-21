2c2f74ca776652e232abca612cc10e89083e5bf2
MathBLL <matheus.bolelabc@gmail.com>
2025-05-21 17:03:45 -0300

    vai corinthians

--- src/poc.js
+++ src/poc.js
@@ -1,13 +1,10 @@
-// Dependência com CVE conhecida
 const express = require('express');
 const app = express();
 
-// Simula um secret hardcoded
 const slackWebhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
 
-// Rota simples
 app.get('/', (req, res) => {
-  res.send('Vulnerável!');
+  res.send('vuln!');
 });
 
 app.listen(3000, () => {
